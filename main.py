13e77fda39582f9bbeae0d4123c6a7987e05d564
Luis Silva <luis.silva1mon@gmail.com>
2024-02-22 17:58:17 -0600

    funcion get_data con ejemplo de uso

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ import database
 import secretos
 import smability
 from datetime import datetime, timedelta
+import csv
 
 #TODO Parsear información que devuelve el API y guardarlo apropiadamente en la base de datos
 
 def main():
+
     # # Ejemplo de como llenar estructuras
     # # sensors.py es un archivo con constantes para cada token eventualmente no será necesario
     # ld = model.LocationData("Lugar Prueba", 19.363166, 99.270527)
@@ -15,9 +17,12 @@ def main():
     # database.post_LocationData(ld)
     # database.post_Sensor(s1)
     
+    sensores = {"P.M. 2.5": 9, "P.M. 10": 8, "Ozono": 7, "CO": 2, "Temperatura": 12, "Humedad": 3}
     d0 = datetime.today() - timedelta(days=90)
     d1 = d0 + timedelta(minutes=5)
-    smability.get_data(secretos.IBERO2, (9,12), d0, d1)
+    data = smability.get_data(secretos.IBERO2, tuple(sensores.values()), d0, d1)
+    data = [data[i][0] for i in range(len(data))]
+
 
     return 0
 
--- smability.py
+++ smability.py
@@ -10,11 +10,13 @@ def get_data(token: str, sensors: tuple, date_0: datetime, date_f: datetime):
     if delta.seconds < 300:
         raise Exception("Delta time is smaller than 5 minutes")
 
+    output = []
     url = 'http://smability.sidtecmx.com/smabilityapi/getdata?'
     for n in sensors:
         query = {'dtstart': start, 'dtend': end, 'token': token, 'idsensor': str(n)}
         response = requests.get(url, params=query)
         data = response.json()
-        print(data)
- 
-    return 0
+        output.append(data)
+
+
+    return output
