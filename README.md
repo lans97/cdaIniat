7ea605b971ae6ab7fc51fd4d601ff6d2d79a63ba
Luis Silva <luis.silva1mon@gmail.com>
2024-09-26 00:30:14 -0600

    docs y otras cosas

--- README.md
+++ README.md
@@ -4,9 +4,9 @@ Pequeña parte del proyecto de calidad del aire para el INIAT
 ## TODOS
 
 - ~~Completar wrapper de base de datos~~
-- Investigar API para cambiar a actualización por eventos
-- Integrar sitios con base de datos*
-- Investigar API de telegram para hacer un bot
+- ~~Investigar API para cambiar a actualización por eventos~~ (equivocaición verbal del técnico)
+- ~~Integrar sitios con base de datos~~
+- ~~Investigar API de telegram para hacer un bot~~
 
 ## Notas
 
@@ -15,8 +15,38 @@ Los tokens de cada sensor se guardan localmente en el archivo sensors.py, no se 
 ## 20/02/2024
 
 ### TODO
-- Wrapper API smability
+- ~~Wrapper API smability~~
 
 ### Notas
-- Aún no se recibe información de la API. Queda pendiente para hacer el wrapper y las funciones que llenen la base
-  de datos adecuadamente
+- Mini wrapper seguro terminado
+
+## Descripción
+
+Pequeño programa para la sincronización de base de datos de Smability con una base de datos local.
+
+Se utilizó el ORM sqlalchemy para el manejo de la base de datos. (https://docs.sqlalchemy.org/en/20/)
+
+No recomiendo almacenar la información de manera local si se tiene acceso a la API de Smability.
+Sin embargo, si es necesario sincronizar, recomiendo realizar un programa que
+pueda sincronizar bloques grandes de datos y verifique si se tiene la información
+de manera local. Una el wrapper de la API de smability debería ayudar con eso.
+también recomiendo hacer una copia de los campos que devuelve la API de Smability
+para mejor compatibilidad.
+
+### Archivos
+
+#### db.sql
+Descripción de la base de datos actual.
+
+#### orm.py
+Declaración de clases para el ORM y su interacción con la base de datos.
+
+#### smability.py
+Wrapper de la API de Smability. Proporciana logging y manejo de excepciones
+básicas para evitar romper el flujo del programa.
+
+#### setup.py
+Ejecuta funciones para incicialización de base de datos y ORM.
+
+#### main.py
+Realiza la sincronización cada 5 minutos.
--- smability.py
+++ smability.py
@@ -1,10 +1,10 @@
 # Wrapper para API de Smability
-from datetime import datetime, timedelta
+from datetime import datetime
 import requests
 import logging
 
 logger = logging.getLogger('smability_logger')
-logger.setLevel(logging.DEBUG)  # Set the logging level as needed
+logger.setLevel(logging.ERROR)  # Set the logging level as needed
 
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
@@ -14,31 +14,37 @@ logger.addHandler(file_handler)
 
 def get_data(token: str, sensors: tuple, date_0: datetime, date_f: datetime):
     """Get sensor measures from date_0 to date_f"""
-    start = date_0.replace(second=0, microsecond=0)
-    end = date_f.replace(second=0, microsecond=0)
-    delta = start - end
-    if delta.seconds < 300:
-        raise Exception("Delta time is smaller than 5 minutes")
-
+    start = date_0.replace(microsecond=0)
+    end = date_f.replace(microsecond=0)
     output = []
     url = 'http://smability.sidtecmx.com/smabilityapi/GetData?'
     for n in sensors:
         query = {'dtstart': start, 'dtend': end, 'token': token, 'idsensor': str(n)}
         logger.info(f"Getting data from {token}: {n}.")
-        response = requests.get(url, params=query)
+        try:
+            response = requests.get(url, params=query)
+        except Exception as e:
+            logger.error(e)
+            return None
         data = response.json()
-        if len(data) != 0:
-            output.append(data[0])
-        else:
+        if data is None:
             output.append({})
-
+        else:
+            if len(data) != 0:
+                output.append(data)
+            else:
+                output.append({})
     return output
 
 def list_sensor(token: str):
     """Get sensor description and IDs from a given device toke from a given device token"""
     url = 'http://smability.sidtecmx.com/smabilityapi/ListSensor?'
     query = {'token': token}
-    response = requests.get(url, params=query)
+    try:
+        response = requests.get(url, params=query)
+    except Exception as e:
+        logger.error(e)
+        return None
     data = response.json()
     return data
 
@@ -46,6 +52,10 @@ def bio_box(token: str):
     """Get last data from sensor"""
     url = 'http://smability.sidtecmx.com/smabilityapi/BioBox?'
     query = {'token': token}
-    response = requests.get(url, params=query)
+    try:
+        response = requests.get(url, params=query)
+    except Exception as e:
+        logger.error(e)
+        return None
     data = response.json()
     return data
